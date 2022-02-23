b8654b89f79f4ba7995c448e8ca66ba16b8c1072
ian-from-dover <ian09hong@gmail.com>
2022-02-23 17:41:02 +0800

    Import Furniture, change material

---
+++ test/Assets/Scripts/Recolour.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Recolour : MonoBehaviour
+{
+    public Material valid;
+    public Material invalid;
+    private List<Material> originalMaterials = new List<Material>();
+    public bool validPlacement;
+    public bool isPlaced = false;
+    private List<GameObject> children = new List<GameObject>();
+    void Start()
+    {
+        if (gameObject.GetComponent<MeshRenderer>() != null) // has no children
+        {
+            originalMaterials.Add(gameObject.GetComponent<MeshRenderer>().material);
+        }
+        else
+        {
+            foreach(Transform child in transform)
+            {
+                Debug.Log(child.gameObject.name);
+                children.Add(child.gameObject);
+                originalMaterials.Add(child.GetComponent<MeshRenderer>().material);
+            }
+        }
+    }
+    void Update()
+    {
+        bool isMaterialSet = validPlacement ? SetMaterial(valid) : SetMaterial(invalid);
+        bool hasBeenPlaced = isPlaced ? SetOriginalMaterial() : false;
+    }
+    
+    private bool SetMaterial(Material m)
+    {
+        if (gameObject.GetComponent<MeshRenderer>() != null) // has no children
+        {
+            gameObject.GetComponent<MeshRenderer>().material = m;
+        }
+        else
+        {
+            foreach(GameObject child in children)
+            {
+                child.GetComponent<MeshRenderer>().material = m;
+            }
+        }
+        return true;
+    }
+    private bool SetOriginalMaterial()
+    {
+        if (gameObject.GetComponent<MeshRenderer>() != null) // has no children
+        {
+            gameObject.GetComponent<MeshRenderer>().material = originalMaterials[0];
+        }
+        else
+        {
+            int i = 0;
+            foreach(GameObject child in children)
+            {
+                child.GetComponent<MeshRenderer>().material = originalMaterials[i];
+                i++;
+            }
+        }
+        return true;
+    }
+}
